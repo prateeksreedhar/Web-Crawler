d7e134937c8418efbfe3728b97894c3449acca27
Prateek Sreedhar Bharadwaj <prateeksreedharbharadwaj@prateeks-mbp.attlocal.net>
2017-09-10 16:02:29 -0400

    extract multiple attributes and initial fix of the post mismatch

--- simplePageExtractor/spiders/simple_spider.py
+++ simplePageExtractor/spiders/simple_spider.py
@@ -24,7 +24,6 @@ class ElementSpider(scrapy.Spider):
         if len(uuurl) > 0:
             yield response.follow(uuurl[0], callback=self.after_main_login)
         else:'''
-        base_url = 'http://forum.nafc.org/'
         url_list = response.xpath('//a[contains(@href, "topic")]/@href').extract()
         
         for url in url_list:
@@ -34,39 +33,45 @@ class ElementSpider(scrapy.Spider):
                 self.log(url)
                 yield response.follow(url, callback=self.parse_post_pages)
 
+    def organize_posts(self, list_of_posts):
+        i  = 0
+        j = i+1
+        clist = []
+        while i < len(list_of_posts):
+           temp = ""
+           while j < len(list_of_posts):
+               if '\n' in list_of_posts[j]:
+                   temp += list_of_posts[j]
+                   j += 1
+               else:
+                   break
+           clist.append(list_of_posts[i]+temp)
+           i = j
+           j = i+1
+        return clist
+
     def parse_post_pages(self, response): 
         self.log("------------------------")
-        #xxx = response.xpath('//td[@class="post-content"]/text()').extract()
-        #self.log(response.xpath('//td[@class="post-content"]/text()').extract())
-        #self.log(response.xpath('//td[@class="post-name"]/a[contains(@title, "view")]/text()').extract())
-        #self.log(response.xpath('//td[@class="post-content"]/text()').extract())
         user_list = response.xpath('//td[@class="post-name"]/a[contains(@title, "view")]/text()').extract()
         posts_list_in_one_page = response.xpath('//td[@class="post-content"]/text()').extract()
-        for each_user,his_post in zip(user_list, posts_list_in_one_page):
+        posts_list_in_one_page_organized = self.organize_posts(posts_list_in_one_page)
+        post_upvotes = response.xpath('//div[@class="postModerateSpan"]/span/span/text()').extract()
+        user_upvotes = response.xpath('//div[@class="postModpointsWrapper"]/a[@class="postModpoints"]/text()').extract()
+        time_stamps = response.xpath('//td[@class="post-date"]/span/@title').extract()
+        #self.log("posttttttttt"+str(posts_list_in_one_page_organized))
+        #self.log(str(len(user_list))+" "+str(len(posts_list_in_one_page_organized))+ " "+str(len(post_upvotes))+" "+str(len(user_upvotes))+" "+str(len(time_stamps)))
+        for post_number, each_user,his_post,post_upvote,user_upvote,time_stamp in zip(post_numbers, user_list, posts_list_in_one_page_organized,post_upvotes, user_upvotes, time_stamps):
             yield{
+                #'POST-NUMBER' : post_number,
                 'USER' : each_user,
                 'POST' : his_post,
+                'POST-UPVOTES' : post_upvote,
+                'USER-UPVOTES' : user_upvote,
+                'TIME-STAMPS' : time_stamp,
             }
         prev_page_link = response.xpath('//a[@class="link-thread-left"]/@href').extract()
         if len(prev_page_link) > 0: 
             yield response.follow(prev_page_link[0],callback=self.parse_post_pages)
-            #self.log(prev_page_link)
-        #page_list = response.xpath('//div[contains(@class,"link-thread-right")]/@href').extract()
-        #self.log(response.body)
-        '''self.log("in parse_data")
-        url_list = response.xpath('//a[contains(@href, "topic")]/@href').extract()
-        for urls in url_list
-        hxs = HtmlXPathSelector(response.body)
-        filename = 'responses3.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-            #f.write(response.body.selector.xpath('//a[contains(@href, "topic")]@href').extract())
-            #f.write(response.xpath('//a').extract())
-        self.log('Saved file' + filename)
-        #self.log("td is" + str(response.css('td.title')))
-        #for title in response.css('td.title'):'''
-
-
 
 if __name__ == "__main__":
     spider = ElementSpider()
