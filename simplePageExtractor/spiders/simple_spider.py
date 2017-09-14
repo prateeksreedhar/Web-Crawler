f900be6d8093e0c351675e183aa8e4974e977494
Prateek Sreedhar Bharadwaj <prateeksreedharbharadwaj@190.169.21.198.tigernet.wifi.dyn.clemson.edu>
2017-09-14 16:48:27 -0400

    Converted all fields to strings instead of a combination of string and list

--- simplePageExtractor/spiders/simple_spider.py
+++ simplePageExtractor/spiders/simple_spider.py
@@ -29,25 +29,28 @@ class ElementSpider(scrapy.Spider):
         for url in url_list:
             split_url = url.split('/')
             if split_url[2] != 'page':
-                self.log("hh")
-                self.log(url)
+                # self.log("hh")
+                # self.log(url)
                 yield response.follow(url, callback=self.parse_post_pages)
 
     def parse_post_pages(self, response): 
         self.log("------------------------")
         posts_ids = response.xpath('//div[contains(@class, "post-container")]/@id').extract()
+        subject = response.xpath('//h1[@class="thread-title-headline left"]/text()').extract()
         for post_id in posts_ids:
             post_container = '//div[contains(@class,"post-container")][@id = "' + post_id
             post_content_in_list = response.xpath( post_container +'"]//tr[2]/td//text()').extract()
             post_content = ''.join(post_content_in_list)
             post_number = response.xpath(post_container + '"]/table[@class="post-table"]//tr[3]/td[@class="post-share"]/text()').extract()
             yield{
-                'POST_NUMBER' : post_number[0][0:8],
-                'USER' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-name"]/a[contains(@href, "profile")]/text()').extract(),
+                'PAGE_NUMBER_OF_THE_POST' : response.xpath('//div[@class="pages-thread-right"]//text()').extract()[-1],
+                'SUBJECT' : subject[0].strip(),
+                'POST_NUMBER' : post_number[0][0:8].replace("\x95",""),
+                'USER' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-name"]/a[contains(@href, "profile")]/text()').extract()[0],
                 'POST' : post_content,
-                'POST-UPVOTES' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-moderate"]/div/span/span/text()').extract(),
-                'USER-UPVOTES' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-avatar"]/div/a[@class="postModpoints"]/text()').extract(),
-                'TIME-STAMPS' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[3]/td[@class="post-date"]/span/@title').extract(),
+                'POST-UPVOTES' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-moderate"]/div/span/span/text()').extract()[0],
+                'USER-UPVOTES' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[1]/td[@class="post-avatar"]/div/a[@class="postModpoints"]/text()').extract()[0],
+                'TIME-STAMPS' : response.xpath(post_container + '"]/table[@class="post-table"]//tr[3]/td[@class="post-date"]/span/@title').extract()[0],
             }
         prev_page_link = response.xpath('//a[@class="link-thread-left"]/@href').extract()
         if len(prev_page_link) > 0: 
